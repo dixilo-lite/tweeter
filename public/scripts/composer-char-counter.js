899d46fc819d9e99e8b50952fe719de40080060a
Jasonn Chang <changjason96@gmail.com>
2025-06-07 21:57:06 -0400

    added word counter logic and styling if word count is over

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,24 @@
+$(document).ready(()=>{
+
+  console.log("I'm ready");
+
+  $("#tweet-text").on('input', function(){
+    const maxLength =140;
+    const textLength = $(this).val().length;
+    const remainingChars =maxLength - textLength;
+    const counterElement = $(this).closest('form').find('.counter');
+    counterElement.text(remainingChars);
+
+    if(remainingChars < 0)
+    {
+      counterElement.css('color', 'red');
+    } else {
+      counterElement.css('color', 'black');
+    }
+
+    console.log($(this).text().length);
+
+});
+
+
+});
